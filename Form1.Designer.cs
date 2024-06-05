a7056c56995647ba4e3f96490872d3c5a0fdd4bf
校凶手 炸 <2453763813@qq.com>
2024-06-05 16:31:32 +0800

    第一次修改

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,6 +43,7 @@ namespace EngineWindowsApplication1
             this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
             this.axSceneControl1 = new ESRI.ArcGIS.Controls.AxSceneControl();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).BeginInit();
@@ -53,15 +54,17 @@ namespace EngineWindowsApplication1
             // axTOCControl1
             // 
             this.axTOCControl1.Location = new System.Drawing.Point(70, 152);
+            this.axTOCControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.axTOCControl1.Name = "axTOCControl1";
             this.axTOCControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axTOCControl1.OcxState")));
-            this.axTOCControl1.Size = new System.Drawing.Size(249, 526);
+            this.axTOCControl1.Size = new System.Drawing.Size(373, 789);
             this.axTOCControl1.TabIndex = 0;
             // 
             // axLicenseControl1
             // 
             this.axLicenseControl1.Enabled = true;
             this.axLicenseControl1.Location = new System.Drawing.Point(1060, 703);
+            this.axLicenseControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.axLicenseControl1.Name = "axLicenseControl1";
             this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
             this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
@@ -71,9 +74,10 @@ namespace EngineWindowsApplication1
             // button1
             // 
             this.button1.BackgroundImage = global::EngineWindowsApplication1.Properties.Resources._01b6a25baafaf7a801213dea5c9797_jpg_2o;
-            this.button1.Location = new System.Drawing.Point(387, 686);
+            this.button1.Location = new System.Drawing.Point(580, 1029);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 40);
+            this.button1.Size = new System.Drawing.Size(160, 60);
             this.button1.TabIndex = 3;
             this.button1.Text = "xitong";
             this.button1.UseVisualStyleBackColor = true;
@@ -82,21 +86,23 @@ namespace EngineWindowsApplication1
             // axToolbarControl1
             // 
             this.axToolbarControl1.Location = new System.Drawing.Point(293, 32);
+            this.axToolbarControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.axToolbarControl1.Name = "axToolbarControl1";
             this.axToolbarControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axToolbarControl1.OcxState")));
-            this.axToolbarControl1.Size = new System.Drawing.Size(265, 28);
+            this.axToolbarControl1.Size = new System.Drawing.Size(397, 28);
             this.axToolbarControl1.TabIndex = 4;
             this.axToolbarControl1.OnMouseDown += new ESRI.ArcGIS.Controls.IToolbarControlEvents_Ax_OnMouseDownEventHandler(this.axToolbarControl1_OnMouseDown);
             // 
             // contextMenuStrip1
             // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.sjbszToolStripMenuItem,
             this.toolStripMenuItem4,
             this.toolStripMenuItem5,
             this.toolStripMenuItem6});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(84, 92);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(92, 124);
             this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
             // 
             // sjbszToolStripMenuItem
@@ -105,59 +111,72 @@ namespace EngineWindowsApplication1
             this.toolStripMenuItem2,
             this.toolStripMenuItem3});
             this.sjbszToolStripMenuItem.Name = "sjbszToolStripMenuItem";
-            this.sjbszToolStripMenuItem.Size = new System.Drawing.Size(83, 22);
+            this.sjbszToolStripMenuItem.Size = new System.Drawing.Size(91, 30);
             this.sjbszToolStripMenuItem.Text = "1";
             this.sjbszToolStripMenuItem.Click += new System.EventHandler(this.sjbszToolStripMenuItem_Click);
             // 
             // toolStripMenuItem2
             // 
             this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(121, 34);
             this.toolStripMenuItem2.Text = "1";
             // 
             // toolStripMenuItem3
             // 
             this.toolStripMenuItem3.Name = "toolStripMenuItem3";
-            this.toolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(121, 34);
             this.toolStripMenuItem3.Text = "2";
             // 
             // toolStripMenuItem4
             // 
             this.toolStripMenuItem4.Name = "toolStripMenuItem4";
-            this.toolStripMenuItem4.Size = new System.Drawing.Size(83, 22);
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(91, 30);
             this.toolStripMenuItem4.Text = "2";
             // 
             // toolStripMenuItem5
             // 
             this.toolStripMenuItem5.Name = "toolStripMenuItem5";
-            this.toolStripMenuItem5.Size = new System.Drawing.Size(83, 22);
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(91, 30);
             this.toolStripMenuItem5.Text = "3";
             // 
             // toolStripMenuItem6
             // 
             this.toolStripMenuItem6.Name = "toolStripMenuItem6";
-            this.toolStripMenuItem6.Size = new System.Drawing.Size(83, 22);
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(91, 30);
             this.toolStripMenuItem6.Text = "4";
             // 
             // axSceneControl1
             // 
             this.axSceneControl1.Location = new System.Drawing.Point(530, 152);
+            this.axSceneControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.axSceneControl1.Name = "axSceneControl1";
             this.axSceneControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axSceneControl1.OcxState")));
-            this.axSceneControl1.Size = new System.Drawing.Size(527, 526);
+            this.axSceneControl1.Size = new System.Drawing.Size(791, 789);
             this.axSceneControl1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1478, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 34);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "º”‘ÿµÿÕº";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::EngineWindowsApplication1.Properties.Resources._01b6a25baafaf7a801213dea5c9797_jpg_2o;
-            this.ClientSize = new System.Drawing.Size(1221, 747);
+            this.ClientSize = new System.Drawing.Size(1832, 1120);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.axSceneControl1);
             this.Controls.Add(this.axToolbarControl1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.axLicenseControl1);
             this.Controls.Add(this.axTOCControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -184,6 +203,7 @@ namespace EngineWindowsApplication1
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
         private ESRI.ArcGIS.Controls.AxSceneControl axSceneControl1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -18,7 +18,6 @@ namespace EngineWindowsApplication1
         private void axLicenseControl1_Enter(object sender, EventArgs e)
         {
 
-
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -44,6 +43,34 @@ namespace EngineWindowsApplication1
         private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog pOpenFileDialog = new OpenFileDialog();
+            pOpenFileDialog.CheckFileExists = true;
+            pOpenFileDialog.Title = "打开ArcScene文档";
+            pOpenFileDialog.Filter = "ArcScene文档(*.sxd)|*.sxd";
+            pOpenFileDialog.Multiselect = false;
+            pOpenFileDialog.RestoreDirectory = true;
+
+            if (pOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string pFileName = pOpenFileDialog.FileName;
+                if (pFileName == "")
+                    return;
+
+                if (axSceneControl1.CheckSxFile(pFileName)) // 检查地图文档是否有效
+                {
+                    axSceneControl1.Scene.ClearLayers();
+                    axSceneControl1.LoadSxFile(pFileName); // 加载地图文档
+                }
+                else
+                {
+                    MessageBox.Show(pFileName + "请加载正确有效的ArcScene文档！", "信息提示");
+                    return;
+                }
+            }
         }
     }
 }
