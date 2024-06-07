bc25ca12fd1da23f4c1fc8831d45de20e5cde925
校凶手 炸 <2453763813@qq.com>
2024-06-07 10:37:37 +0800

    Scene窗口第一次修改

--- Scene窗体.Designer.cs
+++ Scene窗体.Designer.cs
@@ -40,6 +40,7 @@ namespace EngineWindowsApplication1
             this.tIN数据ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.DEM数据ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.坡度分析ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.地形渲染ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.axSceneControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).BeginInit();
@@ -49,17 +50,17 @@ namespace EngineWindowsApplication1
             // 
             // axSceneControl1
             // 
-            this.axSceneControl1.Location = new System.Drawing.Point(395, 148);
+            this.axSceneControl1.Location = new System.Drawing.Point(250, 59);
             this.axSceneControl1.Margin = new System.Windows.Forms.Padding(2);
             this.axSceneControl1.Name = "axSceneControl1";
             this.axSceneControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axSceneControl1.OcxState")));
-            this.axSceneControl1.Size = new System.Drawing.Size(389, 326);
+            this.axSceneControl1.Size = new System.Drawing.Size(512, 431);
             this.axSceneControl1.TabIndex = 0;
             // 
             // axLicenseControl1
             // 
             this.axLicenseControl1.Enabled = true;
-            this.axLicenseControl1.Location = new System.Drawing.Point(11, 56);
+            this.axLicenseControl1.Location = new System.Drawing.Point(741, 0);
             this.axLicenseControl1.Margin = new System.Windows.Forms.Padding(2);
             this.axLicenseControl1.Name = "axLicenseControl1";
             this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
@@ -68,30 +69,31 @@ namespace EngineWindowsApplication1
             // 
             // axToolbarControl1
             // 
-            this.axToolbarControl1.Location = new System.Drawing.Point(98, 72);
+            this.axToolbarControl1.Location = new System.Drawing.Point(0, 27);
             this.axToolbarControl1.Margin = new System.Windows.Forms.Padding(2);
             this.axToolbarControl1.Name = "axToolbarControl1";
             this.axToolbarControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axToolbarControl1.OcxState")));
-            this.axToolbarControl1.Size = new System.Drawing.Size(397, 28);
+            this.axToolbarControl1.Size = new System.Drawing.Size(762, 28);
             this.axToolbarControl1.TabIndex = 2;
             // 
             // axTOCControl1
             // 
-            this.axTOCControl1.Location = new System.Drawing.Point(51, 148);
+            this.axTOCControl1.Location = new System.Drawing.Point(0, 59);
             this.axTOCControl1.Margin = new System.Windows.Forms.Padding(2);
             this.axTOCControl1.Name = "axTOCControl1";
             this.axTOCControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axTOCControl1.OcxState")));
-            this.axTOCControl1.Size = new System.Drawing.Size(315, 306);
+            this.axTOCControl1.Size = new System.Drawing.Size(246, 431);
             this.axTOCControl1.TabIndex = 5;
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.加载数据ToolStripMenuItem,
-            this.坡度分析ToolStripMenuItem});
+            this.坡度分析ToolStripMenuItem,
+            this.地形渲染ToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(846, 25);
+            this.menuStrip1.Size = new System.Drawing.Size(773, 25);
             this.menuStrip1.TabIndex = 6;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -132,11 +134,17 @@ namespace EngineWindowsApplication1
             this.坡度分析ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
             this.坡度分析ToolStripMenuItem.Text = "坡度分析";
             // 
+            // 地形渲染ToolStripMenuItem
+            // 
+            this.地形渲染ToolStripMenuItem.Name = "地形渲染ToolStripMenuItem";
+            this.地形渲染ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.地形渲染ToolStripMenuItem.Text = "地形渲染";
+            // 
             // Scene窗体
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(846, 501);
+            this.ClientSize = new System.Drawing.Size(773, 501);
             this.Controls.Add(this.axTOCControl1);
             this.Controls.Add(this.axToolbarControl1);
             this.Controls.Add(this.axLicenseControl1);
@@ -169,5 +177,6 @@ namespace EngineWindowsApplication1
         private System.Windows.Forms.ToolStripMenuItem tIN数据ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem DEM数据ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 坡度分析ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 地形渲染ToolStripMenuItem;
     }
 }
--- Scene窗体.cs
+++ Scene窗体.cs
@@ -14,6 +14,7 @@ using ESRI.ArcGIS.Carto;
 using System.IO;
 using ESRI.ArcGIS.Geodatabase;
 using ESRI.ArcGIS.DataSourcesFile;
+using ESRI.ArcGIS.DataSourcesRaster;
 
 namespace EngineWindowsApplication1
 {
@@ -25,39 +26,107 @@ namespace EngineWindowsApplication1
             InitializeComponent();
         }
 
-        private string GetDataPath()
-        {
-            string path = null;
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Title = "打开ArcScene文件";
-            openFileDialog.Filter = "ArcScene文件(*.sxd)|*.sxd";
-            openFileDialog.ShowDialog();
-            path = openFileDialog.FileName;
-            return path;
-        }
-
 
         private void arcScene文档ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            string path = GetDataPath();
-            if (axSceneControl1.CheckSxFile(path))
+            OpenFileDialog pOpenFileDialog = new OpenFileDialog();
+            pOpenFileDialog.CheckFileExists = true;
+            pOpenFileDialog.Title = "加载ArcScene文档";
+            pOpenFileDialog.Filter = "ArcScene文档(*.sxd)|*.sxd";
+            pOpenFileDialog.Multiselect = false;
+            pOpenFileDialog.RestoreDirectory = true;
+
+            if (pOpenFileDialog.ShowDialog() == DialogResult.OK)
             {
-                axSceneControl1.LoadSxFile(path);
-                axSceneControl1.SceneGraph.RefreshViewers();
-            }
-            else
-            {
-                MessageBox.Show("请加载正确的ArcScene文件！", "信息提示");
+                string pFileName = pOpenFileDialog.FileName;
+                if (pFileName == "")
+                    return;
+
+                if (axSceneControl1.CheckSxFile(pFileName)) // 检查地图文档是否有效
+                {
+                    axSceneControl1.Scene.ClearLayers();
+                    axSceneControl1.LoadSxFile(pFileName); // 加载地图文档
+                }
+                else
+                {
+                    MessageBox.Show(pFileName + "请加载有效的ArcScene文档！", "信息提示");
+                    return;
+                }
             }
         }
 
         private void DEM数据ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-               
+            ISceneGraph pSG = axSceneControl1.SceneGraph;
+                IScene pS = pSG.Scene;
+                IRasterLayer pRL = new RasterLayerClass();
+                ILayer pLayer;
+
+                OpenFileDialog pOpenFileDialog = new OpenFileDialog();
+                pOpenFileDialog.Title = "加载DEM文档";
+                pOpenFileDialog.Filter = "DEM文档(*.tif)|*.tif";
+
+            if (pOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                    string pPathName = pOpenFileDialog.FileName;
+                    string pPath = pPathName.Substring(0, pPathName.LastIndexOf('\\'));
+                    string fileName = pPathName.Substring(pPath.Length + 1, pPathName.Length - pPath.Length - 1);
+
+                    IWorkspaceFactory pwsf = new RasterWorkspaceFactoryClass();
+                    IRasterWorkspace pRasterWorkspace;
+                if (pwsf.IsWorkspace(pPath))
+                {
+                        pRasterWorkspace = pwsf.OpenFromFile(pPath, 0) as IRasterWorkspace;
+                        IRasterDataset pRasterDataset = pRasterWorkspace.OpenRasterDataset(fileName);
+                        pRL.CreateFromDataset(pRasterDataset);
+                        pLayer = pRL as ILayer;
+                        pS.AddLayer(pLayer, true);
+                        pSG.RefreshViewers();
+                }
+
+                else
+                {
+                        MessageBox.Show(pPath + "请加载有效的DEM文档！", "信息提示");
+                        return;
+                }
+            }
         }
 
         private void tIN数据ToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            ISceneGraph pSceneGraph = this.axSceneControl1.SceneGraph;
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+                IScene pScene = pSceneGraph.Scene;
+                ITinLayer tinLayer = new TinLayerClass();
+                FileInfo fileInfo;
+                string tinPath;
+                IWorkspaceFactory tinWorkspaceFactory = new TinWorkspaceFactoryClass();
+                ITinWorkspace tinWorkspace;
+                ITin tin;
+
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    tinPath = folderBrowserDialog1.SelectedPath;
+                    fileInfo = new FileInfo(tinPath);
+                    if (tinWorkspaceFactory.IsWorkspace(fileInfo.DirectoryName))
+                    {
+                        tinWorkspace = tinWorkspaceFactory.OpenFromFile(fileInfo.DirectoryName, 0) as ITinWorkspace;
+                        //tinWorkspace.OpenTin(fileInfo.DirectoryName);
+                        tin = tinWorkspace.OpenTin(fileInfo.Name);
+                        tinLayer.Dataset = tin;
+                        tinLayer.Visible = true;
+                        tinLayer.Name = fileInfo.Name;
+                        pScene.AddLayer(tinLayer as ILayer, true);
+                        pSceneGraph.RefreshViewers();
+                    }
+                }
+
+                else
+                {
+                MessageBox.Show( "请加载有效的TIN文档！", "信息提示");
+                return;
+                }
+
 
         }
     }
